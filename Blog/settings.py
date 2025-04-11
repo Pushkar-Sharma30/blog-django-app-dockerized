87fdfcad4b37b5016b8b7b7bc9851fcdbe13f5a0
pushkars-repo <gagpushkar@gmail.com>
2025-04-11 22:37:20 +0530

    Temporarily disabled docker-compose for Railway deployment

--- Blog/settings.py
+++ Blog/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)+6^a^2@q2dr-rek(qdp3z_u#%-k9q!zsh3f6x=zhxnu(wvm8$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['127.0.0.1', '192.168.1.44', 'localhost']
 
